760251949c15910b35cf2fe406a8cc0e38e9355c
renzoroel24@gmail.com <enzk0>
2025-03-29 14:23:03 +0800

    TEST: Added failing test for removeTask

--- Todo Manager/test_todomanager.py
+++ Todo Manager/test_todomanager.py
@@ -14,6 +14,20 @@ class Test_todomanager(unittest.TestCase):
                
         self.assertEqual(todo_mgr.tasks[0].description, "Do the dishes.")
         self.assertEqual(todo_mgr.tasks[1].description, "Do the laundry.")
+        
+    def test_removeTask(self):
+        task1 = Task("Do the dishes.")
+        task2 = Task("Do the laundry.")
+        
+        todo_mgr = TodoManager()
+        
+        todo_mgr.tasks.append(task1)
+        todo_mgr.tasks.append(task2)
+               
+        todo_mgr.tasks.removeTask("Do the dishes.")
+        todo_mgr.tasks.removeTask("Do the laundry.")
+        
+        self.assertNotIn(task1.description, todo_mgr.tasks[0].description, "It is in the list")
  
 
 if __name__ == '__main__':
