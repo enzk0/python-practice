532e543e82a3d0605e79557ac887393f17520e70
renzoroel24@gmail.com <enzk0>
2025-03-29 12:48:38 +0800

    TEST: re-edited test cases for addTask

--- Todo Manager/test_todomanager.py
+++ Todo Manager/test_todomanager.py
@@ -1,16 +1,19 @@
 import unittest
+from todomanager import Task
+from todomanager import TodoManager
 
 class Test_todomanager(unittest.TestCase):
     def test_addTask(self):
         task1 = Task("Do the dishes.")
         task2 = Task("Do the laundry.")
         
-        todo_mgr = Todomanager()
+        todo_mgr = TodoManager()
         
         todo_mgr.tasks.append(task1)
         todo_mgr.tasks.append(task2)
-        
-        self.assertEqual(todo_mgr.tasks.index("1"), 0)
+               
+        self.assertEqual(todo_mgr.tasks[0].description, "Do the dishes.")
+        self.assertEqual(todo_mgr.tasks[1].description, "Do the laundry.")
  
 
 if __name__ == '__main__':
